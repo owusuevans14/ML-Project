81bffcdca2aee16ac80759861c430b1ced82baa1
owusuevans14 <60272889+owusuevans14@users.noreply.github.com>
2021-05-03 12:24:04 -0400

    Delete request.py

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/predict_api'
-r = requests.post(url,json={'date':450, 'HOSPITALIZED_COUNT':30000, 'DEATH_COUNT':1000, 'DEATH_COUNT_7DAY_AVG': 2000})
-
-print('TOTAL CASE COUNT WOULD BE {}'.format(r.text))
